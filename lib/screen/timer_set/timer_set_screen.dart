65b486b1bec72a1321494a110baa2176ee54912f
iseruuuuu <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>
2023-01-31 20:44:09 +0900

    add1.0: タイマーセット画面のstateを追加

---
+++ lib/screen/timer_set/timer_set_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:work_record_app/constants/color_constants.dart';
+import 'package:work_record_app/constants/style_constants.dart';
+import 'package:work_record_app/extension/extension.dart';
+import 'package:work_record_app/screen/timer_set/timer_set_screen_state.dart';
+import 'package:work_record_app/screen/timer_set/timer_set_screen_state_notifier.dart';
+
+class TimerSetScreen extends ConsumerWidget {
+  const TimerSetScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(timerSetScreenStateProvider);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: ColorConstants.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: ColorConstants.appBarColor,
+        elevation: 0,
+        title: const Text(
+          '労働タイマー',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: ListTile(
+                tileColor: ColorConstants.white,
+                leading: Text(
+                  '設定時間',
+                  style: StyleConstants.titleStyle,
+                ),
+                trailing: Text(
+                  //TODO 状態が変わるようにする。
+                  state.workingTime.toString(),
+                  style: StyleConstants.timeStyle,
+                ),
+                onTap: () => ref
+                    .read(timerSetScreenStateNotifierProvider.notifier)
+                    .setTime(context),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: TextField(
+                onChanged: (value) => ref
+                    .read(timerSetScreenStateNotifierProvider.notifier)
+                    .onChanged(value),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: InputDecoration(
+                  labelText: '時給',
+                  labelStyle: StyleConstants.titleStyle,
+                  filled: true,
+                  fillColor: ColorConstants.white,
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: SizedBox(
+                width: context.screenWidth,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () => ref
+                      .read(timerSetScreenStateNotifierProvider.notifier)
+                      .onTapTimer(context),
+                  style: StyleConstants.buttonStyle,
+                  child: Text(
+                    'スタート',
+                    style: StyleConstants.buttonTextStyle,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
