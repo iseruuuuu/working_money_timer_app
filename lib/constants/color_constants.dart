d09fb3bd56f979427c65e800f8680c6bb7f45db4
iseruuuuu <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>
2023-01-31 20:42:45 +0900

    add1.0: color_constantsを作成

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,14 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static Color appBarColor = const Color(0xFF4A67AD);
+  static Color backgroundColor = const Color(0xFFF2F2F7);
+  static Color yellow = Colors.yellow;
+  static Color lightBlue = Colors.lightBlue;
+  static Color blue = Colors.blue;
+  static Color grey = Colors.grey;
+  static Color black = Colors.black;
+  static Color white = Colors.white;
+  static Color red = Colors.red;
+}
